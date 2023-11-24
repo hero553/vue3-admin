1c7c230a0844ebbc37d8a7bfad995353ae10349c
LongJu <1055610928@qq.com>
2023-11-24 14:17:10 +0800

    first commit

---
+++ types/shims-vue.d.ts
@@ -0,0 +1,11 @@
+declare module "*.scss" {
+  const scss: Record<string, string>
+  export default scss
+}
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue"
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
